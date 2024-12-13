48bc3e31d4ab83feb296357fbacbdef230e8c883
Danny-Levovsky <149176637+Danny-Levovsky@users.noreply.github.com>
2024-12-13 16:55:44 +0200

    Fixing typo in table

--- src/scripts.js
+++ src/scripts.js
@@ -60,7 +60,7 @@ fileUpload.addEventListener('change', () => {
           </tr>
           <tr>
             <td class="border border-gray-400 dark:border-gray-700 px-4 py-2">00:00:15</td>
-            <td class="border border-gray-400 dark:border-gray-700 px-4 py-2">the transcription will show up as a table after the user uploads a</td>
+            <td class="border border-gray-400 dark:border-gray-700 px-4 py-2">the transcription will show up as a table after the user uploads an audio file</td>
           </tr>
         </tbody>
       </table>
